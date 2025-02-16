d180145880d7061ac609607bf0cb7123634234de
Gavin Huang <ghuang@bunnings.com.au>
2025-02-16 18:55:22 +1100

    hard-code to debug deployment issue

--- article-dryer-nextjs/src/app/page.tsx
+++ article-dryer-nextjs/src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { ArticleSummarizer } from '@/components/ArticleSummarizer';
+// import { ArticleSummarizer } from '@/components/ArticleSummarizer';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ArticleSummarizer />
+      <h1 className="text-4xl font-bold">Article Dryer</h1>
+      {/* <ArticleSummarizer /> */}
     </main>
   );
 }
