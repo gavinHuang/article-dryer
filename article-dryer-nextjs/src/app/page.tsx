b90edbae69ce3b7faa23697960572ccba7976821
Gavin Huang <ghuang@bunnings.com.au>
2025-02-16 19:11:42 +1100

    found the issue and revert unnecessary code

--- article-dryer-nextjs/src/app/page.tsx
+++ article-dryer-nextjs/src/app/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-// import { ArticleSummarizer } from '@/components/ArticleSummarizer';
+import { ArticleSummarizer } from '@/components/ArticleSummarizer';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Article Dryer</h1>
-      {/* <ArticleSummarizer /> */}
-    </main>
+    <ArticleSummarizer />
   );
 }
