a790540d741694033165bc6a101e7bfab1228199
Gavin Huang <shuminghuang@gmail.com>
2025-02-28 00:51:51 +1100

    and more

--- article-dryer-nextjs/src/app/api/articles/url/route.ts
+++ article-dryer-nextjs/src/app/api/articles/url/route.ts
@@ -1,11 +1,15 @@
 import { redis } from '@/lib/redis'
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import type { Article } from '@/lib/redis'
 
-export async function GET(url: string) {
+export async function GET(request: NextRequest) {
   try {
-    const article = await redis.get<Article>(url)
-    return NextResponse.json(article)
+    const url = request.nextUrl.searchParams.get('url');
+    if (!url) {
+      return NextResponse.json({ error: 'URL parameter is required' }, { status: 400 })
+    }
+    const article = await redis.get<Article>(url);
+    return NextResponse.json(article|| null);
   } catch (error) {
     console.error('Failed to fetch articles:', error)
     return NextResponse.json({}, { status: 500 })
